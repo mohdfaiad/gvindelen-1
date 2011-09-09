344fb35cf411f564f7572ab86c4d9b51bb60d547
gvindelen <gvindelen@gmail.com>
2011-09-09 12:55:01 +0000

    настройки

---
+++ Otto/php/index.php
@@ -0,0 +1,62 @@
+<?php
+  session_start();
+  require "libs/GvStrings.php";
+  require "libs/GvHtmlSrv.php";
+  require "libs/utf2win.php";
+  require "settings/mail.inc";
+  require "settings/mysql.inc";
+  $ext = '.php';
+  $template_path = 'template/';
+  
+function extract_searches($template_path, $path) {
+  $path_arr = explode("/", $path);
+  if ($path_arr[0] == null) $path_arr[0] = 'start';
+  $searches = array();
+  $c = count($path_arr);
+  $path = $template_path;
+  $searches[] = $path;
+  for ($i=0; $i<$c; $i++) {
+    $path = $path . $path_arr[$i] . '/';
+    $searches[] = $path;
+  }
+  return $searches;
+}
+  $path = $_GET['path'];
+
+  // проверяем перенаправление страницы
+  $event_fname = $template_path . $path .'/'.'on_redirect_path' . '.php';
+  if (file_exists($event_fname)) {
+    include $event_fname;
+    $path = on_redirect_path($path);
+  }
+  $searches = extract_searches($template_path, $path);
+  
+  
+  function include_template($html, $searches, $template) {
+    global $ext;
+    $template_name = str_replace('<!--#', '', $template);
+    $template_name = str_replace('#-->', $ext, $template_name);
+    $c = count($searches);  
+    for ($i=$c-1; $i>=0; $i--) {
+      $file_name = $searches[$i] . $template_name;
+      if (file_exists($file_name)) {
+        $block = file_get_contents($file_name);
+        return str_replace($template, $block, $html);
+      }  
+    }
+    return str_replace($template, '', $html);
+  }
+  
+  $html = file_get_contents('template/html.php');
+  $templates = explode("\r\n", extract_tags($html, '<!--#', '#-->', "\r\n"));
+  while ($templates[0]) {
+    foreach ($templates as $template_name) {
+      $html = include_template($html, $searches, $template_name);
+    }
+    $templates = explode("\r\n", extract_tags($html, '<!--#', '#-->', "\r\n"));
+  };
+  
+  $fname = 'cache/'.session_id().'.php';
+  file_put_contents($fname, $html);
+  include $fname;
+?>
